2d9cd7a3c1084b9e8b44c39f250d69095b344e4b
ryohei.matsumoto <ec2-user@ip-172-31-23-216.us-east-2.compute.internal>
2018-06-05 06:47:39 +0000

    Assignment of bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,19 +5,24 @@
 <!-- Write content for each page here -->
 <h1>タスク新規作成ページ</h1>
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+     <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                <div clas="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class'=>'form-control']) !!}
+                </div>
     
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-    
-        
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class'=>'form-control']) !!}
+                </div>
        
 
-        {!! Form::submit('追加') !!}
+                {!! Form::submit('追加') !!}
 
-    {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,14 +5,29 @@
 <!-- Write content for each page here -->
 <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
     
-    <p>ステータス: {{ $task->status }}</p>
-    <p>タスク{{ $task->content }}</p>
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->statust }}</td>
+        </tr>
+    </table>
 
     {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
-
-    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
-    {!! Form::close() !!}
-
+    
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-lg-offset-6">
+            {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+                {!! Form::submit('削除') !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
